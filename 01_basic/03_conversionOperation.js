7fb844e728230542865bf9e290bffaa57caa945c
vishu9334 <144935874+vishu9334@users.noreply.github.com>
2024-01-04 08:30:25 +0000

    conversion of dataType

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = 33;
+// let score = "33";
+// let score = "33abc";
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "vishal"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// NaN => NaN is a "not a number"
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// ----------------------------------------------------------------------
+// let isLoggedIn = "vishal";
+// let booleanIsLoogedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoogedIn);
+
+// note:-
+// 1 => true; 0 => false
+// "" => false
+// "vishal" => true
+// -------------------------------------------------------------------------
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
